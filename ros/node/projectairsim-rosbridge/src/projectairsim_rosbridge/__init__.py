adf0417e95099ffc4d04b6bab70cddd944eb5d9d
Ivan <50378504+imuzolev@users.noreply.github.com>
2026-07-28 17:48:28 +0300

    TC: delete ros/node/projectairsim-rosbridge/src/projectairsim_rosbridge/__init__.py

--- ros/node/projectairsim-rosbridge/src/projectairsim_rosbridge/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Copyright (C) Microsoft Corporation. 
-Copyright (C) 2025 IAMAI CONSULTING CORP
-MIT License.
-"""
-
-from .ros_bridge import ProjectAirSimROSBridge
-
-__all__ = ["ProjectAirSimROSBridge"]
