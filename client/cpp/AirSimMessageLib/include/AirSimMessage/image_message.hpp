ff09f23daf2c61f10b0477ffc7867b725b86e071
Ivan <50378504+imuzolev@users.noreply.github.com>
2026-07-28 17:33:34 +0300

    TC: delete client/cpp/AirSimMessageLib/include/AirSimMessage/image_message.hpp

--- client/cpp/AirSimMessageLib/include/AirSimMessage/image_message.hpp
+++
@@ -1,7 +0,0 @@
-// Copyright (C) Microsoft Corporation.  
-// Copyright (C) 2025 IAMAI CONSULTING CORP
-//
-// MIT License. All rights reserved.
-
-#pragma once
-#include <core_sim/message/image_message.hpp>
