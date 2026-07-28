ae7de7622704155296738fc276fedf35d03e8382
Ivan <50378504+imuzolev@users.noreply.github.com>
2026-07-28 17:38:58 +0300

    TC: delete client/python/projectairsim/src/projectairsim/gym_envs/run_sim.sh

--- client/python/projectairsim/src/projectairsim/gym_envs/run_sim.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# python projectairsim_dronelanding_sydney_simulator.py --verbose
-python dronelanding_simulator.py --verbose
-
-# python dronelanding_sydney_simulator.py --verbose
-
-# python dronelanding_sydney_with_perception_simulator.py --verbose
